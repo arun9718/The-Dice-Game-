1f20876a9eaf5cc72d5ca8679e33eef205560b35
arun9718 <66300702+arun9718@users.noreply.github.com>
2020-08-30 20:32:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1=Math.floor(Math.random()*6+1);
+console.log(randomNumber1);
+
+var randomdice="images/dice"+randomNumber1+".png";
+document.querySelector("img.img1").setAttribute("src",randomdice);
+
+
+var randomNumber2=Math.floor(Math.random()*6+1);
+var randomdice2="images/dice"+randomNumber2+".png";
+
+document.querySelector("img.img2").setAttribute("src",randomdice2);
+
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw";
+}
